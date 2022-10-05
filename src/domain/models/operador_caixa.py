2704a9e0363d35157a499272226efe197cc57602
Bruno Petri <47029565+petribrn@users.noreply.github.com>
2022-10-04 21:27:32 -0300

    Models adjustments (#5)

--- src/domain/models/operador_caixa.py
+++ src/domain/models/operador_caixa.py
@@ -2,5 +2,5 @@ from funcionario import Funcionario
 
 class OperadorCaixa(Funcionario):
 
-    def __init__(self, nome: str, cpf: int):
-        super().__init__(nome, cpf, "operador")
+    def __init__(self, nome: str, cpf: str, email: str, telefone: str, senha: str):
+        super().__init__(nome, cpf, email, telefone, senha, "operador")
