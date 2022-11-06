4146e89a1379b23f9bbe648f085f6b6169f9a40c
francostavares <francostavares2003@gmail.com>
2022-11-05 21:06:10 -0300

    corrige confirmação de fechamento de caixa

--- src/domain/views/tela_abstrata.py
+++ src/domain/views/tela_abstrata.py
@@ -27,12 +27,12 @@ class Tela(ABC):
     def show_message(self, titulo: str, msg):
         sg.Popup(titulo, msg)
 
+    def show_form_confirmation(self, titulo: str, msg):
+        return sg.PopupOKCancel(titulo, msg)
+
     def read(self):
         botao, valores = self.__window.read()
         return botao, valores
 
     def update(self, key: str, value):
         self.__window[key].update(value)
-
-    def tela_opcoes(self):
-        return self.open()
